10f35030b123ab3970da9e33668b1aac360b165d
Willems Davy <joyrider3774@hotmail.com>
2020-05-20 20:29:59 +0200

    add conversion script batch file

---
+++ plugins/conversion scripts/convert_all_pdfs.bat
@@ -0,0 +1,17 @@
+@echo off
+for /D %%D in ("*") do if exist "%%D\*.pdf" call :ConvertFileToPNG "%%D"
+goto :EOF
+
+:ConvertFileToPNG
+cd "%~1"
+echo **********************************************************
+Echo converting PDF TO PNG in "%~1"
+echo **********************************************************
+for %%F in ("*.pdf") do (
+    echo ---------------------------------
+	..\pdftopng.exe "%%F" "%%~nF"
+)
+cd ..
+
+goto :EOF
+    	
